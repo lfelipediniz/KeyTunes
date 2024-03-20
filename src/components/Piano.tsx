78f6d165dd07686b00acf62e596cbb1514c150bf
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-03-19 21:33:11 -0300

    update

---
+++ src/components/Piano.tsx
@@ -0,0 +1,315 @@
+import React from "react";
+
+const Piano: React.FC = () => {
+  return (
+    <div>
+      {/* A vitural piano component */}
+        <button className="whiteKey" id="0a">
+          A
+        </button>
+        <button className="blackKey" id="0as">
+          As
+        </button>
+        <button className="whiteKey" id="0b">
+          B
+        </button>
+
+        <button className="whiteKey" id="1a">
+          A
+        </button>
+        <button className="blackKey" id="1as">
+          As
+        </button>
+        <button className="whiteKey" id="1b">
+          B
+        </button>
+        <button className="whiteKey" id="1c">
+          C
+        </button>
+        <button className="blackKey" id="1cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="1d">
+          D
+        </button>
+        <button className="blackKey" id="1ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="1e">
+          E
+        </button>
+        <button className="whiteKey" id="1f">
+          F
+        </button>
+        <button className="blackKey" id="1fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="1g">
+          G
+        </button>
+        <button className="blackKey" id="1gs">
+          Gs
+        </button>
+
+        <button className="whiteKey" id="2a">
+          A
+        </button>
+        <button className="blackKey" id="2as">
+          As
+        </button>
+        <button className="whiteKey" id="2b">
+          B
+        </button>
+        <button className="whiteKey" id="2c">
+          C
+        </button>
+        <button className="blackKey" id="2cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="2d">
+          D
+        </button>
+        <button className="blackKey" id="2ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="2e">
+          E
+        </button>
+        <button className="whiteKey" id="2f">
+          F
+        </button>
+        <button className="blackKey" id="2fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="2g">
+          G
+        </button>
+        <button className="blackKey" id="2gs">
+          Gs
+        </button>
+
+        <button className="whiteKey" id="3a">
+          A
+        </button>
+        <button className="blackKey" id="3as">
+          As
+        </button>
+        <button className="whiteKey" id="3b">
+          B
+        </button>
+        <button className="whiteKey" id="3c">
+          C
+        </button>
+        <button className="blackKey" id="3cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="3d">
+          D
+        </button>
+        <button className="blackKey" id="3ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="3e">
+          E
+        </button>
+        <button className="whiteKey" id="3f">
+          F
+        </button>
+        <button className="blackKey" id="3fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="3g">
+          G
+        </button>
+        <button className="blackKey" id="3gs">
+          Gs
+        </button>
+
+        <button className="whiteKey" id="4a">
+          A
+        </button>
+        <button className="blackKey" id="4as">
+          As
+        </button>
+        <button className="whiteKey" id="4b">
+          B
+        </button>
+        <button className="whiteKey" id="4c">
+          C
+        </button>
+        <button className="blackKey" id="4cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="4d">
+          D
+        </button>
+        <button className="blackKey" id="4ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="4e">
+          E
+        </button>
+        <button className="whiteKey" id="4f">
+          F
+        </button>
+        <button className="blackKey" id="4fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="4g">
+          G
+        </button>
+        <button className="blackKey" id="4gs">
+          Gs
+        </button>
+
+        <button className="whiteKey" id="5a">
+          A
+        </button>
+        <button className="blackKey" id="5as">
+          As
+        </button>
+        <button className="whiteKey" id="5b">
+          B
+        </button>
+        <button className="whiteKey" id="5c">
+          C
+        </button>
+        <button className="blackKey" id="5cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="5d">
+          D
+        </button>
+        <button className="blackKey" id="5ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="5e">
+          E
+        </button>
+        <button className="whiteKey" id="5f">
+          F
+        </button>
+        <button className="blackKey" id="5fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="5g">
+          G
+        </button>
+
+        <button className="whiteKey" id="6a">
+          A
+        </button>
+        <button className="blackKey" id="6as">
+          As
+        </button>
+        <button className="whiteKey" id="6b">
+          B
+        </button>
+        <button className="whiteKey" id="6c">
+          C
+        </button>
+        <button className="blackKey" id="6cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="6d">
+          D
+        </button>
+        <button className="blackKey" id="6ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="6e">
+          E
+        </button>
+        <button className="whiteKey" id="6f">
+          F
+        </button>
+        <button className="blackKey" id="6fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="6g">
+          G
+        </button>
+        <button className="blackKey" id="6gs">
+          Gs
+        </button>
+
+        <button className="whiteKey" id="7a">
+          A
+        </button>
+        <button className="blackKey" id="7as">
+          As
+        </button>
+        <button className="whiteKey" id="7b">
+          B
+        </button>
+        <button className="whiteKey" id="7c">
+          C
+        </button>
+        <button className="blackKey" id="7cs">
+          Cs
+        </button>
+        <button className="whiteKey" id="7d">
+          D
+        </button>
+        <button className="blackKey" id="7ds">
+          Ds
+        </button>
+        <button className="whiteKey" id="7e">
+          E
+        </button>
+        <button className="whiteKey" id="7f">
+          F
+        </button>
+        <button className="blackKey" id="7fs">
+          Fs
+        </button>
+        <button className="whiteKey" id="7g">
+          G
+        </button>
+        <button className="blackKey" id="7gs">
+          Gs
+        </button>
+
+        <button className="whiteKey" id="8c">
+          C
+        </button>
+
+      <style>
+        {`
+        .whiteKey, .blackKey {
+            border: 1px solid #000;
+            cursor: pointer;
+            position: relative;
+            }
+        
+        .whiteKey {
+            background-color: #fff;
+            height: 100px;
+            width: 30px;
+            margin-right: 1px;
+            z-index: 1;
+            color: #000;   
+            padding-top: 60px;
+            }
+        
+        .blackKey {
+            background-color: #000;
+            height: 60px;
+            width: 20px;
+            margin-left: -10px;
+            z-index: 2;
+            position: absolute;
+            font-size: 0.8em;
+            }
+        
+        .blackKey:not(:last-child) {
+            margin-right: 40px;
+            }
+                      
+    `}
+      </style>
+    </div>
+  );
+};
+
+export default Piano;
