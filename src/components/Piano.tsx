ebe3435f49a07562318de5377d403009b323dd12
Luiz Felipe <61145881+lfelipediniz@users.noreply.github.com>
2024-03-20 20:53:17 -0300

    add keyboard function

--- src/components/Piano.tsx
+++ src/components/Piano.tsx
@@ -1,43 +1,73 @@
-'use client'; // this is a client-side component
+'use client';
 import React, { useState, useEffect } from 'react';
 
+// indicates the mapping of keys to note IDs
+type KeyMap = {
+  [key: string]: string;
+};
+
 const Piano: React.FC = () => {
-  const [keyMap, setKeyMap] = useState({});
+  
+  // state to store the key map
+  const [keyMap, setKeyMap] = useState<KeyMap>({});
 
   useEffect(() => {
-    // fetch the default keys from the JSON file
-    // and set the state with the key map
-    fetch('/defaultKeys.json')
-      .then((res) => res.json())
-      .then((data) => setKeyMap(data))
-      .catch((error) => console.error('Erro ao carregar as teclas do piano', error));
-  }, []);
+    // load the key map from the defaultKeys.json file
+    const loadKeyMap = async () => {
+      try {
+        const response = await fetch('/defaultKeys.json');
+        const data = await response.json();
+        setKeyMap(data);
+      } catch (error) {
+        console.error('Error loading piano keys', error);
+      }
+    };
+
+    loadKeyMap();
+
+    // add event listener to handle keydown events
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const keyCombo = `${event.shiftKey ? 'Shift + ' : ''}${event.code}`;
+      const noteId = keyMap[keyCombo];
+      if (noteId) {
+        playAudio(noteId);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    // cleanup the event listener
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [keyMap]); // run the effect only when the keyMap changes
 
   const playAudio = (noteId: string) => {
     const audio = new Audio(`/notes/${noteId}.wav`);
     audio.play().catch(error => console.error("Erro ao tentar reproduzir o áudio", error));
   };
 
-  // function that handles the click event on the piano keys
-  const handleKeyClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const target = event.target as HTMLElement;
-    if (target.dataset.noteId) {
-      playAudio(target.dataset.noteId);
+  const handleKeyClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const noteId = event.currentTarget.dataset.noteId;
+    if (noteId) {
+      playAudio(noteId);
     }
   };
 
   return (
-    <div onClick={handleKeyClick} className="flex relative select-none">
+    <div className="flex relative select-none">
       {Object.entries(keyMap).map(([key, noteId]: any) => (
         <div key={key} className="relative">
-          <button
-            className={`border border-black cursor-pointer flex justify-center items-end ${noteId.includes('s') ? 'bg-black text-white h-24 w-5 z-20 absolute top-0 left-1/2 transform -translate-x-1/2' : 'bg-white text-black h-40 w-8 mr-1 z-10'}`}
-            data-note-id={noteId}
-            onClick={() => playAudio(noteId)}
-            style={{ paddingBottom: '1rem' }}
-          >
-            {noteId}
-          </button>
+        <button
+          key={key}
+          className={`border border-black cursor-pointer flex justify-center items-end ${noteId.includes('s') ? 'bg-black text-white h-24 w-5 z-20 absolute top-0 left-1/2 transform -translate-x-1/2' : 'bg-white text-black h-40 w-8 mr-1 z-10'}`}
+          data-note-id={noteId}
+          onClick={handleKeyClick}
+          style={{ paddingBottom: '1rem' }}
+          
+        >
+          {noteId}
+        </button>
         </div>
       ))}
     </div>
